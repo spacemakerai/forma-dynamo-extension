f9b8b583937f5e10f4da734fe22077b00f0795ab
Håvard Høiby <havard.hoiby@autodesk.com>
2023-11-01 19:11:38 -0400

    fix: restyle error page

--- analyses/dynamo/src/floatingpanel/pages/components/ErrorView.tsx
+++ analyses/dynamo/src/floatingpanel/pages/components/ErrorView.tsx
@@ -1,116 +1,118 @@
-import { useCallback, useState, useEffect } from "preact/hooks";
-import dynamoIconUrn from "../../icons/dynamo.png";
+import { useCallback, useState } from "preact/hooks";
+
 import dynamoRevitExtensionUrl from "../../../assets/DynamoPlayerExtension2.18.7z?url";
+
+function TabHeaders({ label, onClick, isActive }: any) {
+  return (
+    <span
+      style={{
+        cursor: "pointer",
+        margin: "5px",
+        borderBottom: isActive ? "2px solid blue" : "2px solid gray",
+      }}
+      onClick={onClick}
+    >
+      {label}
+    </span>
+  );
+}
 
 export function ErrorView() {
   const downloadDynamoPlayerExtension = useCallback(() => {
     window.open(dynamoRevitExtensionUrl, "_blank");
   }, []);
-  const downloadDynamoPlayerExtensionRevit = useCallback(() => {
-    window.open(dynamoRevitExtensionUrl, "_blank");
-  }, []);
 
   const [app, setApp] = useState("sandbox");
-  const [expanded, setExpanded] = useState(false);
-
-  const [isVisible, setIsVisible] = useState(false);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsVisible(true);
-    }, 1000);
-    return () => clearTimeout(timeout);
-  });
-
-  function AnimatedDots() {
-    const [dots, setDots] = useState(0);
-
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setDots((dots) => (dots + 1) % 4);
-      }, 500);
-      return () => clearInterval(interval);
-    }, []);
-
-    return <span>{Array(dots).fill(".").join("")}</span>;
-  }
 
   return (
-    <>
-      <div style={{ height: "40px" }}>
-        {isVisible && (
-          <>
-            Dynamo not responding (retrying <AnimatedDots />)
-            <button onClick={() => setExpanded(!expanded)}>More</button>
-          </>
-        )}
-      </div>
-      {expanded && (
-        <div>
-          This can either be because Dynamo is <b>busy</b>, or because Dynamo is
-          not installed.
+    <div>
+      :warn: Not able to connect to Dynamo.
+      <br />
+      <br />
+      - Are Dynamo and the FormaDynamo extension installed, and is Dynamo
+      running? If so, check Dynamo for any open dialogs that may be blocking.
+      <br />
+      - Dynamo not installed: Follow installation directions below
+      <br />
+      <br />
+      <div style={{ width: "100%", height: "1px", backgroundColor: "gray" }} />
+      <br />
+      <TabHeaders
+        onClick={() => setApp("sandbox")}
+        label={"Dynamo Sandbox"}
+        isActive={app === "sandbox"}
+      />
+      <TabHeaders
+        onClick={() => setApp("revit")}
+        label={"Revit"}
+        isActive={app === "revit"}
+      />
+      <br />
+      <br />
+      {app === "sandbox" ? (
+        <>
+          <div>
+            Follow these directions to install Dynamo Sandbox and FormaDynamo
+            extension.
+          </div>
           <br />
+          <ol>
+            <li>
+              1. Download Dynamo 2.81.0 from{" "}
+              <a
+                href="https://dyn-builds-data.s3-us-west-2.amazonaws.com/DynamoCoreRuntime2.18.1.zip"
+                target="_blank"
+              >
+                daily builds
+              </a>
+            </li>
+            <br />
+            <li>
+              2. Download{" "}
+              <a href={"#"} onClick={downloadDynamoPlayerExtension}>
+                FormaDynamo.7z
+              </a>
+            </li>
+            <br />
+            <li>
+              3. Extract Dynamo 2.18 using <a href={"#"}>7zip</a> to a folder of
+              your choise.
+            </li>
+            <br />
+            <li>
+              4. Extract FormaDynamo.7z to{" "}
+              <span style={{ background: "#80808040", fontFamily: "Monaco" }}>
+                %AppData%\Dynamo\Dynamo Core\2.18\packages
+              </span>
+            </li>
+            <br />
+            <li>5. Open DynamoSandbox.exe from the Dynamo 2.18 folder</li>
+          </ol>
+        </>
+      ) : (
+        <ol>
+          <li>
+            1. Make sure you have Revit 2024.1 installed with Dynamo 2.18.1
+          </li>
           <br />
-          <button onClick={() => setApp("sandbox")}>Dynamo Sandbox</button>
-          <button onClick={() => setApp("revit")}>Revit</button>
+          <li>
+            2. Download{" "}
+            <a href={"#"} onClick={downloadDynamoPlayerExtension}>
+              FormaDynamo.7z
+            </a>
+          </li>
           <br />
-          Install dynamo with these instructions:
+          <li>
+            4. Extract FormaDynamo.7z to{" "}
+            <span style={{ background: "#80808040", fontFamily: "Monaco" }}>
+              %AppData%\Dynamo\Dynamo Revit\2.18\packages
+            </span>
+          </li>
           <br />
-          <br />
-          {app === "sandbox" ? (
-            <ol>
-              <li>
-                1. Download and extract Dynamo 2.81.0 from{" "}
-                <a
-                  href="https://dyn-builds-data.s3-us-west-2.amazonaws.com/DynamoCoreRuntime2.18.1.zip"
-                  target="_blank"
-                >
-                  Daily Builds
-                </a>
-              </li>
-              <br />
-              <li>
-                2. Download{" "}
-                <button onClick={downloadDynamoPlayerExtension}>
-                  DynamoPlayerExtension2.18.7z
-                </button>
-              </li>
-              <br />
-              <li>
-                3. Extract DynamoPlayerExtension2.18.7z to{" "}
-                <span style={{ background: "#80808040", fontFamily: "Monaco" }}>
-                  %AppData%\Dynamo\Dynamo Core\2.18\packages
-                </span>
-              </li>
-              <br />
 
-              <li>4. Open DynamoSandbox.exe from downloaded Dynamo 2.18</li>
-            </ol>
-          ) : (
-            <ol>
-              <li>
-                1. Make sure you have Revit 2024.1 installed with Dynamo 2.18.1
-              </li>
-              <br />
-              <li>
-                2. Download{" "}
-                <button onClick={downloadDynamoPlayerExtensionRevit}>
-                  DynamoPlayerPackage2.18.7z
-                </button>
-              </li>
-              <br />
-              <li>
-                3. Extract DynamoPlayerPackage2.18.7z to{" "}
-                <span style={{ background: "#80808040", fontFamily: "Monaco" }}>
-                  %AppData%\Dynamo\Dynamo Revit\2.18\packages
-                </span>
-              </li>
-              <br />
-
-              <li>4. Open Dynamo from Revit</li>
-            </ol>
-          )}
-        </div>
+          <li>4. Open Dynamo from Revit</li>
+        </ol>
       )}
-    </>
+    </div>
   );
 }
