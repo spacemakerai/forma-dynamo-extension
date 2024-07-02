1afff6880a8354a1adf7b734e464fdb206b0735d
Håvard Høiby <havard.hoiby@autodesk.com>
2024-07-02 18:41:20 +0200

    feat: add drop-zone ui

--- src/pages/DaasApp.tsx
+++ src/pages/DaasApp.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useRef, useState } from "preact/hooks";
+import { useCallback, useMemo, useState } from "preact/hooks";
 import Dynamo from "../service/dynamo";
 import sphereAreaGraph from "../assets/spherearea.json";
 import { LocalScript } from "./LocalScript";
@@ -6,6 +6,8 @@ import { Forma } from "forma-embedded-view-sdk/auto";
 import { DaasServerInfo } from "./DaaSServerInfo";
 import { Health } from "../components/Health/Health";
 import { useDynamoConnector } from "../DynamoConnector";
+import { Import } from "../assets/icons/Import";
+import Logo from "../assets/Logo.png";
 
 export type JSONGraph = {
   type: "JSON";
@@ -29,6 +31,8 @@ const urls: Record<string, string> = {
 export function DaasApp() {
   const [graph, setGraph] = useState<JSONGraph | undefined>(undefined);
 
+  const [dropped, setDropped] = useState<any | undefined>(undefined);
+
   const sampleGraphs = useSampleGraphs();
 
   const daas = useMemo(() => {
@@ -38,11 +42,10 @@ export function DaasApp() {
     });
   }, []);
 
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-
   // TODO: we don't need to handle multiple files
   const onDrop = useCallback<JSX.DragEventHandler<HTMLDivElement>>(
     (event) => {
+      console.log(123);
       event.preventDefault();
       if (event.dataTransfer?.items) {
         // Use DataTransferItemList interface to access the file(s)
@@ -51,10 +54,9 @@ export function DaasApp() {
           if (item.kind === "file") {
             const file = item.getAsFile();
             if (file) {
-              file.text().then((t) => {
-                if (textAreaRef.current) {
-                  textAreaRef.current.value = t;
-                }
+              file.text().then((text) => {
+                const graph = JSON.parse(text);
+                setDropped(graph);
               });
             }
           }
@@ -62,31 +64,24 @@ export function DaasApp() {
       } else {
         // Use DataTransfer interface to access the file(s)
         [...(event.dataTransfer?.files || [])].forEach((file) => {
-          file.text().then((t) => {
-            if (textAreaRef.current) {
-              textAreaRef.current.value = t;
-            }
+          file.text().then((text) => {
+            const graph = JSON.parse(text);
+            setDropped(graph);
           });
         });
       }
     },
-    [textAreaRef],
+    [setDropped],
   );
 
-  const onLoadTextAreaContentsAsGraph = useCallback(() => {
-    if (!textAreaRef.current) {
-      return;
-    }
-
-    const contents = textAreaRef.current.value;
-    const graph: JSONGraph = {
+  const openDroppedGraph = useCallback(() => {
+    setGraph({
       id: "2",
       type: "JSON",
-      name: "Manually input graph",
-      graph: JSON.parse(contents),
-    };
-    setGraph(graph);
-  }, []);
+      name: dropped.Name,
+      graph: dropped,
+    });
+  }, [setGraph, dropped]);
 
   const dynamoLocal = useDynamoConnector();
 
@@ -95,15 +90,47 @@ export function DaasApp() {
       <Health daas={daas} local={dynamoLocal.state} />
       {!graph && (
         <>
-          <h2>Upload a graph or load a sample graph:</h2>
-          <div id="dropzone" style={{ display: "flex" }} onDrop={onDrop}>
-            <textarea
-              placeholder="Drag-n-drop or paste the contents of a .dyn file here"
-              ref={textAreaRef}
-              style={{ flex: 1, minHeight: "80px" }}
-            />
+          <h4>My own graph</h4>
+          <div id="dropzone">
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "60px",
+                border: "1px dashed gray",
+                borderRadius: "4px",
+              }}
+              onDragOver={(e) => {
+                console.log("dragover");
+                e.preventDefault();
+              }}
+              onDrop={onDrop}
+            >
+              <Import />
+              <b>Drag & Drop</b>
+              Files to import (.dyn)
+            </div>
           </div>
-          <weave-button onClick={onLoadTextAreaContentsAsGraph}>Load</weave-button>
+          {dropped && (
+            <div
+              style={{
+                margin: "16px 8px 16px 8px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <div style={{ display: "flex", flexDirection: "colum" }}>
+                <img style={{ marginRight: "4px" }} src={Logo} />
+                <div style={{ height: "24px", alignContent: "center" }}>{dropped.Name}.dyn</div>
+              </div>
+              <weave-button variant="solid" onClick={openDroppedGraph}>
+                Open
+              </weave-button>
+            </div>
+          )}
           <h4>Sample Graphs</h4>
           {sampleGraphs.map((script) => {
             return (
