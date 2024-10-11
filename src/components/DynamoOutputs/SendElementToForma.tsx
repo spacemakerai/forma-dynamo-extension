4004cfa8e8ab40aee331b32f76b5674de00ce3a3
Lars Smeby <lars.smeby@autodesk.com>
2024-10-11 15:18:14 +0200

    add preview to sendElementToForma

--- src/components/DynamoOutputs/SendElementToForma.tsx
+++ src/components/DynamoOutputs/SendElementToForma.tsx
@@ -1,15 +1,59 @@
-import { useCallback, useMemo, useState } from "preact/hooks";
+import { Dispatch, StateUpdater, useCallback, useEffect, useMemo, useState } from "preact/hooks";
 import { Forma } from "forma-embedded-view-sdk/auto";
 import { captureException } from "../../util/sentry.ts";
 import { Output } from "./types.tsx";
 import { Transform, Urn } from "forma-elements";
+import { Visibility } from "../../icons/Visibility.tsx";
+import * as UUID from "uuid";
 
 type SendElementDTO = {
   urn: Urn;
   transform: Transform;
 };
 
-function useAddElement(elements: SendElementDTO[]) {
+function useTogglePreview(elements: SendElementDTO[], isPreviewActive: boolean) {
+  console.log(isPreviewActive);
+  const [isPreviewLoading, setIsPreviewLoading] = useState(false);
+  const [isPreviewVisible, setIsPreviewVisible] = useState(false);
+  const [renderId] = useState(UUID.v4());
+
+  useEffect(() => {
+    if (!isPreviewLoading && isPreviewVisible !== isPreviewActive) {
+      console.log("når blir denne kalt?");
+      setIsPreviewLoading(true);
+      if (!isPreviewVisible) {
+        Forma.experimental.render.element
+          .update({
+            id: renderId,
+            elements: elements
+              .filter((element) => !!element)
+              .map((element) => ({ urn: element.urn, transform: element.transform })),
+          })
+          .then(() => {
+            setIsPreviewVisible(true);
+            setIsPreviewLoading(false);
+          });
+      } else {
+        Forma.experimental.render.element.remove({ id: renderId }).then(() => {
+          setIsPreviewVisible(false);
+          setIsPreviewLoading(false);
+        });
+      }
+    }
+  }, [
+    isPreviewLoading,
+    isPreviewActive,
+    setIsPreviewLoading,
+    elements,
+    renderId,
+    isPreviewVisible,
+  ]);
+}
+
+function useAddElement(
+  elements: SendElementDTO[],
+  togglePreview: (newPreviewActiveState: boolean) => void,
+) {
   const [isAdding, setIsAdding] = useState(false);
   const [isAdded, setIsAdded] = useState(false);
   const add = useCallback(async () => {
@@ -18,24 +62,29 @@ function useAddElement(elements: SendElementDTO[]) {
       await Promise.all(
         elements.map(({ urn, transform }) => Forma.proposal.addElement({ urn, transform })),
       );
+      togglePreview(false);
       setIsAdded(true);
     } catch (e) {
       captureException(e, "Failed to add element");
     } finally {
       setIsAdding(false);
     }
-  }, [elements]);
+  }, [elements, togglePreview]);
 
   return { add, isAdding, isAdded };
 }
 
 function PreviewAndAdd({ value }: { value: string[] }) {
+  const [isPreviewActive, setIsPreviewActive] = useState(true);
+
   const elements: SendElementDTO[] = useMemo(
     () => value.map((value) => JSON.parse(value)).flat(1),
     [value],
   );
 
-  const { add, isAdding, isAdded } = useAddElement(elements);
+  useTogglePreview(elements, isPreviewActive);
+
+  const { add, isAdding, isAdded } = useAddElement(elements, setIsPreviewActive);
 
   return (
     <div style={{ display: "flex" }}>
@@ -43,6 +92,10 @@ function PreviewAndAdd({ value }: { value: string[] }) {
       <weave-button variant="outlined" disabled={isAdding} onClick={add}>
         Add
       </weave-button>
+      <Visibility
+        onClick={() => setIsPreviewActive(!isPreviewActive)}
+        isVisible={isPreviewActive}
+      />
     </div>
   );
 }
