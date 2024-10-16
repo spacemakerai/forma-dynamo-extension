bae14735e22cedc7ebae51a5b5ac3306f40c275d
Håvard Høiby <havard.hoiby@autodesk.com>
2024-10-16 08:57:43 -0700

    feat: adding information about signups

--- src/pages/LocalFileList.tsx
+++ src/pages/LocalFileList.tsx
@@ -4,6 +4,8 @@ import { SampleFiles } from "../components/SampleFiles.tsx";
 import { CurrentOpenGraphPrompt } from "../components/CurrentOpenGraphPrompt.tsx";
 import { DynamoService, FolderGraphInfo } from "../service/dynamo.ts";
 
+const teaseDaas = new URLSearchParams(window.location.search).has("ext:tease-daas");
+
 type SetScript = (script: FolderGraphInfo) => void;
 
 function LoadingScriptList() {
@@ -154,6 +156,48 @@ export function LocalFileList({
         }}
       />
       <br />
+      {teaseDaas && (
+        <>
+          <div
+            style={{
+              border: "1px solid var(--divider-lightweight)",
+              borderRadius: "5px",
+              padding: "0 16px 16px 16px",
+            }}
+          >
+            <h3>Dynamo as a Service for Forma - Early Access</h3>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <div>
+                For now this is released under a closed beta.{" "}
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://dynamobim.org/dynamo-as-a-service-powers-up-dynamo-player-in-forma"
+                >
+                  Read more this on our blog
+                </a>{" "}
+                and sign up now to get early access!
+              </div>
+              <div style={{ marginLeft: "32px" }}>
+                <weave-button
+                  variant="solid"
+                  onClick={() =>
+                    window.open(
+                      "https://feedback.autodesk.com/key/DynamoForma",
+                      "_blank",
+                      "noreferrer",
+                    )
+                  }
+                >
+                  Sign up
+                </weave-button>
+              </div>
+            </div>
+          </div>
+          <br />
+          <br />
+        </>
+      )}
       <div
         style={{
           display: "flex",
